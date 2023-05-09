29a0e4e1fd50d9525731221fd22ea5241d27632f
Erick Vicente <vicenteerick777@gmail.com>
2023-05-08 19:45:34 -0600

    Agregando archivos iniciales css, js y carpeta img

---
+++ script.js
@@ -0,0 +1,5 @@
+//Carga de Archivos Dinamicamente CSS
+document.head.innerHTML +=  `
+<link rel="stylesheet" href="./style.css${getTime}">`;
+document.head.innerHTML +=  `
+<link rel="stylesheet" href="./normalize.css${getTime}">`;
